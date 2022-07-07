74683c3c36b3f68b85b3566bf8a2b814cd57cfb0
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-06 23:15:42 -0700

    work on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
-const userController = {
-    
+module.exports ={
+    // get all users
+    // get single user
+    // post new user
+    // put to update user
+    // delete to remove user
 }
