6ef85afa16935ec24a38e2f0baf4c52339318389
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-19 04:38:33 -0700

    cleaned up comments

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -21,26 +21,3 @@ router
     .delete(deleteThought);
 
 module.exports = router;
-
-// * `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
-
-// ```json
-// // example data
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// }
-// ```
-
-// * `PUT` to update a thought by its `_id`
-
-// * `DELETE` to remove a thought by its `_id`
-
-// ---
-
-// **`/api/thoughts/:thoughtId/reactions`**
-
-// * `POST` to create a reaction stored in a single thought's `reactions` array field
-
-// * `DELETE` to pull and remove a reaction by the reaction's `reactionId` value
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -82,29 +82,7 @@ router.post('/logout', (req, res) => {
 
 module.exports = router;
 
-// **`/api/users`**
 
-// * `GET` all users
-
-// * `GET` a single user by its `_id` and populated thought and friend data
-
-// * `POST` a new user:
-
-// ```json
-// // example data
-// {
-//   "username": "lernantino",
-//   "email": "lernantino@gmail.com"
-// }
-// ```
-
-// * `PUT` to update a user by its `_id`
-
-// * `DELETE` to remove user by its `_id`
-
-// **BONUS**: Remove a user's associated thoughts when deleted.
-
-// ---
 
 // **`/api/users/:userId/friends/:friendId`**
 
