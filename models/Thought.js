09b18a9d12ad453f0ccb108ae0531dbd4aa58b5c
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-19 04:32:51 -0700

    reaction count

--- models/Thought.js
+++ models/Thought.js
@@ -29,31 +29,16 @@ const thoughtSchema = new Schema(
     {
         toJSON: {
             getters: true,
+            virtuals: true,
         },
         id: false,
     }
 );
 
+// reaction count
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
 const Thought = model('thought', thoughtSchema);
-module.exports = {Thought, thoughtSchema}
-
-// * `thoughtText`
-//   * String
-//   * Required
-//   * Must be between 1 and 280 characters
-
-// * `createdAt`
-//   * Date
-//   * Set default value to the current timestamp
-//   * Use a getter method to format the timestamp on query
-
-// * `username` (The user that created this thought)
-//   * String
-//   * Required
-
-// * `reactions` (These are like replies)
-//   * Array of nested documents created with the `reactionSchema`
-
-// **Schema Settings**:
-
-// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+module.exports = {Thought, thoughtSchema};
